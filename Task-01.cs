7f8480dc2404d2b49d268ee7cc5788aa7a70646e
ir-shaad0812 <irshad.aalam0812@gmail.com>
2025-11-04 10:01:21 +0545

    Task 01: Variables and String Interpolation

---
+++ Task-01.cs
@@ -0,0 +1,9 @@
+class UserInfo
+{
+    static void Main()
+    {
+        string userName = "MSSIR";
+        int luckyNumber = 69;
+        Console.WriteLine($"Hello, {userName}! Your lucky number is {luckyNumber}.");
+    }
+}
